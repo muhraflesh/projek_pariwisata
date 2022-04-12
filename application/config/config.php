33c6ca1a21b658a43f302799d12b1c77bf488944
muhraflesh <muhraflesh@gmail.com>
2022-04-12 11:51:31 +0700

    make it better

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/web_pariwisata/';
+// $config['base_url'] = 'http://localhost/web_pariwisata/';
+$config['base_url'] = 'https://pariwisatadeswita.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
